8b598fe0bc00f9f86b4d505aa6997be8dd6e0209
ScopeWraith <scopewraith.lastwar@gmail.com>
2025-08-11 07:34:42 -0400

    Update main.js

--- code/code/js/main.js
+++ code/code/js/main.js
@@ -10,7 +10,7 @@ import { auth } from './firebase-config.js';
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
 import { initializeAllEventListeners } from './event-listeners.js';
 import { setupInitialUI, showPage, buildMobileNav, updateUIForLoggedInUser, updateUIForLoggedOutUser, renderSkeletons, toggleSubNav } from './ui/ui-manager.js';
-import { setupAllListeners, detachAllListeners, fetchInitialData } from './firestore.js';
+import { setupAllListeners, detachAllListeners, fetchInitialData, setupUnverifiedPlayersListener } from './firestore.js';
 import { setupPresenceManagement } from './presence.js';
 import { setCallbacks } from './state.js';
 
