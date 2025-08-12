d1dfe34e8ddaed84849207b687d50294f49fd73f
ScopeWraith <scopewraith.lastwar@gmail.com>
2025-08-11 20:03:46 -0400

    2

--- code/code/js/event-listeners.js
+++ code/code/js/event-listeners.js
@@ -8,7 +8,7 @@ import { handleLoginSubmit, handleForgotPassword, handleRegistrationNext, handle
 import { handlePlayerSettingsSubmit } from './ui/player-settings-ui.js';
 import { handlePostBack, handleThumbnailSelection, handlePostSubmit} from './ui/post-ui.js';
 import { applyPlayerFilters } from './ui/players-ui.js';
-import { handleSendMessage, handleDeleteMessage, handleNotificationAction, addFriend, removeFriend, sendPrivateMessage, setupChatListeners, toggleReaction, togglePostReaction, handleImageAttachment, handleFullscreenMessageSend  } from './firestore.js'; // Added handleFullscreenMessageSend
+import { handleSendMessage, handleDeleteMessage, handleNotificationAction, addFriend, removeFriend, sendPrivateMessage, setupChatListeners, toggleReaction, togglePostReaction, handleImageAttachment, handleFullscreenMessageSend  } from './firestore.js';
 import { positionEmojiPicker } from './utils.js';
 
 export function initializeAllEventListeners() {
